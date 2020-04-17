525507f278c0a9f0a7c16445d3e985375929155a
AmitPr <17amitprasad@gmail.com>
2020-04-17 13:23:44 -0400

    Added support for subsections, Edited README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Today In History
 Today In History was a short coding project, scraping wikipedia for unique events that occured on that day, and showing a random one to the user. All information is courtesy of [Wikipedia](http://www.wikipedia.org)!
+## How It works
+Using jQuery's [getJSON](https://api.jquery.com/jQuery.getJSON/) function, and the wikipedia [REST API](https://en.wikipedia.org/api/rest_v1/), a JSON object is obtained, containing the wikipedia page for the day. For example, if today were to be January 1, then the JSON would be retrieved from:
+> https://en.wikipedia.org/api/rest_v1/page/mobile-sections/January_1  
+
+The JSON response contains two important sections: A header, which acts as a Table of Contents for the page, and the Content, which contains the text of each section. The JSON is parsed, first searching for the "Events" section (and any subsections, which is the case for January 1), before jumping to those sections. It then extracts the plain text facts from the HTML representation (using a simpe regex expression), as well as getting rid of the footnote links that wikipedia tends to include before finally displaying the result.  
--- fetch.js
+++ fetch.js
@@ -2,22 +2,35 @@ facts = []
 const d = new Date()
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 today = monthNames[d.getMonth()] +' ' + d.getDate()
-$.getJSON('https://en.wikipedia.org/api/rest_v1/page/mobile-sections/' + monthNames[d.getMonth()] + '_' + d.getDate(), function (data) {
-    id = -1
+date = document.getElementsByClassName("date")[0]
+date.innerHTML = today
+date.href='https://en.wikipedia.org/wiki/' + monthNames[d.getMonth()]+'_'+d.getDate()+'#Events'
+$.getJSON('https://en.wikipedia.org/api/rest_v1/page/mobile-sections/'+ monthNames[d.getMonth()] + '_' + d.getDate() , function (data) {
+    id = [-1]
+    inside = false
     $.each(data.lead.sections, function (i, section) {
         if (section.hasOwnProperty('anchor')) {
             if (section.anchor == 'Events') {
-                id = section.id - 1
+                inside=true
+                id[0] = section.id - 1
+            }
+            else if(inside && section.toclevel==2){
+                id.push(section.id-1)
+            }else if(inside){
+                inside=false
             }
         }
     })
-    bullets = $.trim(data.remaining.sections[id].text).split('\n')
-    $.each(bullets, function (i, fact) {
-        f = fact.replace(/<[^>]*>/g, '').replace(/\[[^ ]*\]/g, '').split(/–(.+)/)
-        f[0] = $.trim(f[0])
-        f[1] = $.trim(f[1])
-        facts.push(f)
+    $.each(id,function(i,curID){
+        bullets = $.trim(data.remaining.sections[curID].text).split('\n')
+        $.each(bullets, function (i, fact) {
+            f = fact.replace(/<[^>]*>/g, '').replace(/\[[^ ]*\]/g, '').split(/–(.+)/)
+            f[0] = $.trim(f[0])
+            f[1] = $.trim(f[1])
+            facts.push(f)
+        })
     })
+    console.log(facts.length + ' facts parsed -- Ready!')
     refreshFact()
 })
 function refreshFact() {
@@ -27,11 +40,3 @@ function refreshFact() {
     evt = document.getElementsByClassName("event")[0]
     evt.innerHTML = fact[1]
 }
-$(document).ready(function () {
-    date = document.getElementsByClassName("date")[0]
-    date.innerHTML = today
-    date.href='https://en.wikipedia.org/wiki/' + monthNames[d.getMonth()]+'_'+d.getDate()+'#Events'
-    $('#bg').on('mousedown', function (e) {
-        refreshFact();
-    });
-})
